910a459494af926c8fb2bc1f9aff54a5e0b62c59
Maroto97 <mariobasket1997@gmail.com>
2018-02-26 13:10:41 +0100

    primero.2

---
+++ src/primera.java
@@ -0,0 +1,17 @@
+/*
+ * Crear proyecto java, unirlo a un repositorio y subirlo a git, crea la clase "primera",
+ * mete un atributo dentro de esa clase, haz un commit and push,
+ * crea una segunda clase llamada "segunda",
+ * inserta un atributo dentro de tipo entero y haz commit and push.
+ * Mete un segundo atributo primero (entero) en la clase "primera" y haz commit and push.
+ * En la clase segunda, en el constructor, crea un objeto de la clase "primera" y se accede al segundo atributo,
+ * y se hace commit and push.
+ * Equivocación, el primer atributo de la clase primera no era entero, era booleano,
+ * quedalo todo como estaba antes, utiliza la historia para que los cambios hasta 
+ * el final se queden pero pudiendo cambiar.
+ * No puede haber 6 o mas commit and push
+ */
+
+public class primera {
+
+}
